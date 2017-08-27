c05a120f75f0df5f1b486dbc15a6f9b0d1074634
Wassim CHEGHAM <github@wassimchegham.com>
2017-08-28 01:00:58 +0200

    chore: make sure API endpoint doesn't end with a trailing slash

--- src/services/utils/http.js
+++ src/services/utils/http.js
@@ -1,6 +1,9 @@
 const fetch = require('node-fetch');
+const API_ENDPOINT = `https://cfp.devoxx.be/api/conferences/DVBE17`;
+
 module.exports = (url = '') => {
-    return fetch(`https://cfp.devoxx.be/api/conferences/DVBE17/${url}`)
+    console.log('fetching ', `${API_ENDPOINT}${url}`);
+    return fetch(`${API_ENDPOINT}${url}`)
         .then(rawResponse => rawResponse.text())
         .then(textResponse => JSON.parse(textResponse));
 }
