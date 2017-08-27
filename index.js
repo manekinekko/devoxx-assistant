cf0676d147865c624ac83e20bb016bf6aed4bd15
Wassim CHEGHAM <github@wassimchegham.com>
2017-08-27 14:01:38 +0200

    chore: refactor intent names

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const ApiAiAssistant = require('actions-on-google').ApiAiAssistant;
 const fetch = require('node-fetch');
 
 const intents = [
-    '_devoxxx.find_by_topic',
-    '_devoxxx.find_by_topic_more',
-    '_devoxxx.find_by_topic_next',
+    'find_by_topic',
+    // 'find_by_topic_more',
+    // 'find_by_topic_next',
+    'list_topics'
 ];
 
 exports.agent = function(request, response) {
     console.log("start");
     let assistant = new ApiAiAssistant({ request, response });
     let actionMap = new Map();
-    intents.forEach(intent => actionMap.set(intent, require(`./${intents}`)));
+    intents.forEach(intent => actionMap.set(intent, require(`./src/intents/${intent}`)(app)));
     assistant.handleRequest(actionMap);
 };
