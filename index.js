4d603ed3a9fa7a8c341a628643d595fa5b3cc6c0
Wassim CHEGHAM <github@wassimchegham.com>
2017-10-16 11:18:23 +0200

    chore: update to SDK v1.5

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const ApiAiAssistant = require('actions-on-google').ApiAiAssistant;
+const DialogflowApp = require('actions-on-google').DialogflowApp;
 const fetch = require('node-fetch');
 
 const intents = [
     'find_by_topic',
     // 'find_by_topic_more',
     // 'find_by_topic_next',
-    'list_topics'
+    'list_topics',
+    'actions.intent.OPTION'
 ];
 
 exports.agent = function(request, response) {
     console.log("start");
-    let assistant = new ApiAiAssistant({ request, response });
+    let assistant = new DialogflowApp({ request, response });
     let actionMap = new Map();
     intents.forEach(intent => actionMap.set(intent, require(`./src/intents/${intent}`)));
     assistant.handleRequest(actionMap);
