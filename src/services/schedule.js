c52bb98f6fd3b8d1c73967b12d120f5e9448b69c
Wassim CHEGHAM <github@wassimchegham.com>
2017-10-16 11:19:53 +0200

    chore: wip on the schedule API

--- src/services/schedule.js
+++ src/services/schedule.js
@@ -8,7 +8,7 @@ module.exports = () => {
         return Promise.resolve(__CACHE);
     }
 
-    schedules = devoxxApi( /* root api */ ).then(data => {
+    return devoxxApi( /* root api */ ).then(data => {
 
         // SchduleObject = {
         //   proposalTypesId: [''],
@@ -18,20 +18,15 @@ module.exports = () => {
         //   locale: ['']
         // }
 
-        // get all days
-        const conferenceDyas = data.days;
-
         // get the schedule of each day
-        return conferenceDyas.map(day => {
-            return devoxxApi(`schedules/${day}`)
-        });
-    });
+        const conferenceDyas = data.days.map(day => devoxxApi(`/schedules/${day.toLowerCase()}`));
 
-    // run once and cache the result
-    return Promise.all(schedules)
-        .then(data => data.map(d => d.slots))
-        .then(slots => {
-            __CACHE = [].concat(...slots);
-            return __CACHE;
-        });
+        // run once and cache the result
+        return Promise.all(conferenceDyas)
+            .then(data => data.map(d => d.slots))
+            .then(slots => {
+                __CACHE = [].concat(...slots);
+                return __CACHE;
+            });
+    });
 }
