3526c23c847311d6045226b8d66b40f9da43c5a7
Wassim CHEGHAM <github@wassimchegham.com>
2017-10-31 03:17:09 +0100

    chore: add missing guard

--- src/services/utils/array.js
+++ src/services/utils/array.js
@@ -9,6 +9,9 @@ function range(array, start, end) {
 }
 
 function shuffle(array) {
+  if (array.length === 0) {
+    return [];
+  }
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
