42893e137e3413d4da7448df273281584dfec6d9
Wassim CHEGHAM <github@wassimchegham.com>
2017-11-01 01:51:06 +0100

    chore: add a mock http api

--- src/services/utils/http.js
+++ src/services/utils/http.js
@@ -1,11 +1,35 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 const API_ENDPOINT = `https://cfp.devoxx.be/api/conferences/DVBE17`;
 
-module.exports = (url = '') => {
-    return fetch(`${API_ENDPOINT}${url}`)
-        .then(rawResponse => rawResponse.text())
-        .then(textResponse => JSON.parse(textResponse))
-        .catch(e => {
-          throw new Error(`An error has occured when fetching ${url}.`);
-        });
+function devoxxApi(url = "") {
+  return fetch(`${API_ENDPOINT}${url}`)
+    .then(rawResponse => rawResponse.text())
+    .then(textResponse => JSON.parse(textResponse))
+    .catch(e => {
+      throw new Error(`An error has occured when fetching ${url}.`);
+    });
 }
+
+function devoxxApiMock(path) {
+  try {
+    return new Promise((resolve, reject) => {
+      const content = require("fs").readFile(
+        `src/services/__mock__/${path}.json`,
+        (error, content) => {
+          if (error) {
+            reject(error);
+          }
+          resolve(JSON.parse(content));
+        }
+      );
+    });
+  }
+  catch(e) {
+    throw new Error(`An error has occured when readin ${path}.`);    
+  }
+}
+
+module.exports = {
+  devoxxApi,
+  devoxxApiMock
+};
