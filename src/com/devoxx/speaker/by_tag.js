875c4705675fd1d398e0c4dfcd455434beb0ea2b
Wassim CHEGHAM <github@wassimchegham.com>
2017-11-03 04:36:03 +0100

    chore: add missing function "take"

--- src/com/devoxx/speaker/by_tag.js
+++ src/com/devoxx/speaker/by_tag.js
@@ -1,47 +1,56 @@
 const { getSpeakersByTag } = require("src/services/speaker");
+const { take } = require("src/services/utils/array");
 
 module.exports = app => {
   const tag = app.getArgument("tag");
-  getSpeakersByTag(tag).then(speakers => {
-    if (speakers.length === 0) {
-      app.tell(
-        `It seems there is no person speaking about ${tag}. You might want to try with a different topic.`
-      );
-    } else {
-      if (app.hasScreen()) {
-        // show a carousel with the tracks icons
-
-        let list = app.buildList(`Here are the people speaking about ${tag}:`);
-
-        speakers.forEach(speaker => {
-          const name = `${speaker.firstName} ${speaker.lastName}`;
-          list = list.addItems(
-            app
-              .buildOptionItem(name, [name])
-              .setTitle(name)
-              .setDescription(speaker.bio)
-              .setImage(speaker.avatarURL, name)
-          );
-          console.log("builing list with speaker", speaker);
-        });
-
-        app.setContext("speaker-bio", 1);
-        app.askWithList(
-          `Here are the people speaking about ${tag}. Which speaker are you interested in?`,
-          list
+  getSpeakersByTag(tag)
+    .then(speakers => {
+      if (speakers.length === 0) {
+        app.tell(
+          `It seems there is no person speaking about ${tag}. You might want to try with a different topic.`
         );
       } else {
-        const randomSpeakers = take(speakers, 3);
-        const speakersNames = randomSpeakers
-          .map(speaker => `${speaker.firstName} ${speaker.lastName}`)
-          .join(", ");
+        if (app.hasScreen()) {
+          // show a carousel with the tracks icons
 
-        app.setContext("speakers", 2, randomSpeakers);
+          let list = app.buildList(
+            `Here are the people speaking about ${tag}:`
+          );
 
-        app.ask(
-          `I found ${speakers.length} speakers presenting about ${tag}. Here are 3 of them: ${speakersNames}. Which speaker are you interested in?`
-        );
+          speakers.forEach(speaker => {
+            if (speaker) {
+              const name = `${speaker.firstName} ${speaker.lastName}`;
+              list = list.addItems(
+                app
+                  .buildOptionItem(name, [name])
+                  .setTitle(name)
+                  .setDescription(speaker.bio)
+                  .setImage(speaker.avatarURL, name)
+              );
+            }
+            app.debug("builing list with speaker UUID", speaker.uuid);
+          });
+
+          app.setContext("speaker-bio", 1);
+          app.askWithList(
+            `Here are the people speaking about ${tag}. Which speaker are you interested in?`,
+            list
+          );
+        } else {
+          const randomSpeakers = take(speakers, 3);
+
+          const speakersNames = randomSpeakers
+            .map(speaker => `${speaker.firstName} ${speaker.lastName}`)
+            .join(", ");
+
+          app.ask(
+            `I found ${speakers.length} speakers presenting about ${tag}. Here are 3 of them: ${speakersNames}. Would you like to know something else?`
+          );
+        }
       }
-    }
-  });
+    })
+    .catch(e => {
+      app.error(e);
+      app.ask(`${e}`);
+    });
 };
